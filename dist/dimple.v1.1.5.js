3da94507251495d7b16339cd22466e8b2a3879f5
Moses Ngone <moses@mospired.com>
2014-03-21 16:25:40 -0400

    replaced the resctangle legend with a circle

--- dist/dimple.v1.1.5.js
+++ dist/dimple.v1.1.5.js
@@ -1656,11 +1656,11 @@ var dimple = {
         this.horizontalAlign = horizontalAlign;
         // Help: http://github.com/PMSI-AlignAlytics/dimple/wiki/dimple.legend#wiki-shapes
         this.shapes = null;
-        // Copyright: 2013 PMSI-AlignAlytics
-        // License: "https://github.com/PMSI-AlignAlytics/dimple/blob/master/MIT-LICENSE.txt"
-        // Source: /src/objects/legend/methods/_draw.js
-        // Render the legend
-        this._draw = function (duration) {
+         // Copyright: 2013 PMSI-AlignAlytics
+         // License: "https://github.com/PMSI-AlignAlytics/dimple/blob/master/MIT-LICENSE.txt"
+         // Source: /src/objects/legend/methods/_draw.js
+         // Render the legend
+        this._draw = function(duration) {
 
             // Create an array of distinct color elements from the series
             var legendArray = this._getEntries(),
@@ -1690,24 +1690,26 @@ var dimple = {
                 .data(legendArray)
                 .enter()
                 .append("g")
-                    .attr("class", "legend")
-                    .attr("opacity", 0);
+                .attr("class", "legend")
+                .attr("opacity", 0);
 
             // Add text into the group
             theseShapes.append("text")
-                .attr("id", function (d) { return "legend_" + d.key; })
+                .attr("id", function(d) {
+                    return "legend_" + d.key;
+                })
                 .attr("class", "legendText")
                 .text(function(d) {
                     return d.key;
                 })
-                .call(function () {
+                .call(function() {
                     if (!self.chart.noFormats) {
                         this.style("font-family", "sans-serif")
                             .style("font-size", (self.chart._heightPixels() / 35 > 10 ? self.chart._heightPixels() / 35 : 10) + "px")
                             .style("shape-rendering", "crispEdges");
                     }
                 })
-                .each(function () {
+                .each(function() {
                     var b = this.getBBox();
                     if (b.width > maxWidth) {
                         maxWidth = b.width;
@@ -1717,11 +1719,16 @@ var dimple = {
                     }
                 });
 
-            // Add a rectangle into the group
-            theseShapes.append("rect")
-                .attr("class", "legendKey")
-                .attr("height", keyHeight)
-                .attr("width",  keyWidth);
+            // Replace the rectangle with a circle
+            // theseShapes.append("rect")
+            //     .attr("class", "legendKey")
+            //     .attr("height", keyHeight)
+            //     .attr("width",  keyWidth);
+            //
+
+
+            theseShapes.append("circle")
+                .attr("class", "legendKey");
 
             // Expand the bounds of the largest shape slightly.  This will be the size allocated to
             // all elements
@@ -1730,7 +1737,7 @@ var dimple = {
 
             // Iterate the shapes and position them based on the alignment and size of the legend
             theseShapes
-                .each(function (d) {
+                .each(function(d) {
                     if (runningX + maxWidth > self._widthPixels()) {
                         runningX = 0;
                         runningY += maxHeight;
@@ -1739,24 +1746,45 @@ var dimple = {
                         d3.select(this).remove();
                     } else {
                         d3.select(this).select("text")
-                            .attr("x", (self.horizontalAlign === "left" ? self._xPixels() + keyWidth + 5 + runningX : self._xPixels() + (self._widthPixels() - runningX - maxWidth) + keyWidth + 5))
-                            .attr("y", function () {
+                            .attr("x", (self.horizontalAlign === "left" ? self._xPixels() + keyWidth + runningX - 3 : self._xPixels() + (self._widthPixels() - runningX - maxWidth) + keyWidth + 5))
+                            .attr("y", function() {
                                 // This was previously done with dominant-baseline but this is used
                                 // instead due to browser inconsistancy.
                                 return self._yPixels() + runningY + this.getBBox().height / 1.65;
                             })
                             .attr("width", self._widthPixels())
                             .attr("height", self._heightPixels());
+
+
                         d3.select(this).select("rect")
                             .attr("class", "legend legendKey")
                             .attr("x", (self.horizontalAlign === "left" ? self._xPixels() + runningX : self._xPixels() + (self._widthPixels() - runningX - maxWidth)))
                             .attr("y", self._yPixels() + runningY)
                             .attr("height", keyHeight)
-                            .attr("width",  keyWidth)
-                            .style("fill", function () { return dimple._helpers.fill(d, self.chart, d.series); })
-                            .style("stroke", function () { return dimple._helpers.stroke(d, self.chart, d.series); })
-                            .style("opacity", function () { return dimple._helpers.opacity(d, self.chart, d.series); })
+                            .attr("width", keyWidth)
+                            .style("fill", function() {
+                                return dimple._helpers.fill(d, self.chart, d.series);
+                            })
+                            .style("stroke", function() {
+                                return dimple._helpers.stroke(d, self.chart, d.series);
+                            })
+                            .style("opacity", function() {
+                                return dimple._helpers.opacity(d, self.chart, d.series);
+                            })
                             .style("shape-rendering", "crispEdges");
+
+                        // Use the provided values for teh rectangle
+                        d3.select(this).select("circle")
+                            .attr("cx", (self.horizontalAlign === "left" ? self._xPixels() + runningX : self._xPixels() + (self._widthPixels() - runningX - maxWidth)))
+                            .attr("cy", self._yPixels() + runningY + 4)
+                            .attr("r", 6)
+                            .style("fill", function() {
+                                return dimple._helpers.fill(d, self.chart, d.series);
+                            })
+                            .style("stroke", function() {
+                                return dimple._helpers.stroke(d, self.chart, d.series);
+                            });
+
                         runningX += maxWidth;
                     }
                 });
@@ -1771,7 +1799,6 @@ var dimple = {
             // Assign them to the public property for modification by the user.
             this.shapes = theseShapes;
         };
-
         // Copyright: 2013 PMSI-AlignAlytics
         // License: "https://github.com/PMSI-AlignAlytics/dimple/blob/master/MIT-LICENSE.txt"
         // Source: /src/objects/legend/methods/_getEntries.js
